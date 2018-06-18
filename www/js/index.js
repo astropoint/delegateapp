94336a6378f2288d355b37cb172d449bf8ca6af3
james_rooney@hotmail.com <james_rooney@hotmail.com>
2018-06-18 22:13:51 +0100

    Changes to 18/6/18

--- www/js/index.js
+++ www/js/index.js
@@ -13,13 +13,12 @@ $(document).ready(function(){
 			}, 10000);
 });
 
-
-
-//ondeviceready - NOTE FUNCTIONS AND VARS IN HERE ARE IN LOCAL SCOPE
-
+var destinationType;
 function onDeviceReady(){
 		attendees = new Object(); //this can be done on declaration
-}  //END OF ONDEVICEREADY
+		pictureSource=navigator.camera.PictureSourceType;
+		destinationType=navigator.camera.DestinationType;
+}
 
 var apiURL = "https://reg.bookmein2.com/api/api.php";
 var isInternet = true;
@@ -86,6 +85,15 @@ $(document).on('click', '.returnhome', function(){
 	
 });
 
+$(document).on('click',"#gotomeetingbutton",function() {
+	var clicked_button_Id = e.target.id;
+	$('#selectedmeeting').val(clicked_button_Id);
+	showMeetingDetails();
+	location.href = "#delegateMeetingDetails";
+});
+
+
+
 $(document).on('click',".conf_btn_delete",function(e){
 	var thisid = $(this).attr('id').split("_")[1];
 	clearConference(thisid);
@@ -101,6 +109,16 @@ $(document).on('click',".conf_btn",function() {
 	localStorage.setItem('curapikey', curapikey);
 	refreshConferenceData(thisid, true);
 	location.href = "#delegateHome";
+});
+
+$(document).on('click', '#arrange_meeting_btn', function(e){
+	e.preventDefault();
+	$('#person_profile_meeting_schdlr_div').slideDown();
+});
+
+$(document).on('click', '#cancel_schdlr_btn', function(e){
+	e.preventDefault();
+	$('#person_profile_meeting_schdlr_div').slideUp();
 });
 
 $(document).on('click',"#log_in_btn",function(e){
@@ -156,6 +174,7 @@ $(document).on('click',"#log_in_btn",function(e){
 					localStorage.setItem("conf_"+numconferences+"_image", response.data.conf_id);
 					localStorage.setItem("conf_"+numconferences+"_desc", response.data.conf_desc);
 					localStorage.setItem("conf_"+numconferences+"_name", response.data.conf_name);
+					localStorage.setItem("conf_"+numconferences+"_meetingslist", "");
 					localStorage.setItem("conf_"+numconferences+"_active", 1);
 					
 					localStorage.setItem("numconferences", numconferences);
@@ -175,7 +194,7 @@ $(document).on('click',"#log_in_btn",function(e){
 	}
 });
 
-$(document).on('click',"#updt_profile_btn",function() { 
+$(document).on('click',"#updt_profile_btn",function() {
 
 	if(isInternet){
 		var thisapikey = localStorage.getItem("conf_"+curconference+"_apikey");
@@ -185,7 +204,10 @@ $(document).on('click',"#updt_profile_btn",function() {
 		data += "&first_name="+encodeURIComponent($('#delegateUser_Profile').find('input[name="f_name"]').val());
 		data += "&last_name="+encodeURIComponent($('#delegateUser_Profile').find('input[name="s_name"]').val());
 		data += "&organisation="+encodeURIComponent($('#delegateUser_Profile').find('input[name="org"]').val());
-
+		if( typeof(receiptPicture)!==null && receiptPicture!=null && receiptPicture!=''){
+			data += "&profileimage="+encodeURIComponent(receiptPicture);
+		}
+		$('#piccyoutput').val(receiptPicture);
 		$.ajax({
 			url: apiURL,
 			data: data,
@@ -193,6 +215,7 @@ $(document).on('click',"#updt_profile_btn",function() {
 			type: 'post',
 		}).done(function(response){
 			refreshProfilePage();
+			//$('#piccyoutput').html(response); 
 			if(response.success){
 				$('#profilestatusresponse').html("Succesfully updated profile");
 			}
@@ -202,6 +225,70 @@ $(document).on('click',"#updt_profile_btn",function() {
 	}
 	
 });
+
+$(document).on('click', '#send_meeting_btn', function(e){
+	e.preventDefault();
+	$('#otherprofilestatusresponse').hide();
+	if(isInternet){
+		alert($('#createmeeting_date').val());
+		if($('#createmeeting_date').val()!=''){
+			var thisapikey = localStorage.getItem("conf_"+curconference+"_apikey");
+			var data = "action=requestmeeting&apikey="+thisapikey;
+			var meetingtime = $('#createmeeting_date').val();
+			var meetingtime = $('#createmeeting_location').val();
+			var meetingtime = $('#createmeeting_message').val();
+			
+			data += "&meeting_date="+encodeURIComponent($('#createmeeting_date').val());
+			data += "&location="+encodeURIComponent($('#schdlr_lctn').val());
+			data += "&message="+encodeURIComponent($('#schdlr_msg').val());
+			data += "&receiverref="+encodeURIComponent($('#selectedattendeeref').val());
+			$.ajax({
+				url: apiURL,
+				data: data,
+				dataType: "json",
+				type: 'post',
+			}).done(function(response){
+				refreshProfilePage();
+				//$('#piccyoutput').html(response); 
+				if(response.success){
+					$('#otherprofilestatusresponse').html("Succesfully updated profile");
+				}
+			});
+		}else{
+			$('#otherprofilestatusresponse').show();
+			$('#otherprofilestatusresponse').html("You need to pick a date for the meeting");
+			
+		}
+	}else{
+		$('#otherprofilestatusresponse').show();
+		$('#otherprofilestatusresponse').html("You need an active internet connection to request a meeting");
+	}
+});
+
+//sets target attendeeref when you click a person, then loads data and redirects to profile
+$(document).on('click',".clickbuttongetgetperson",function(e) {     
+	var clicked_button_Id = e.target.id;
+	getAttendeeProfile(clicked_button_Id);
+	location.href = "#delegatePerson_Profile";
+}); 
+
+$(document).on('click',"#imagepickerprofile",function() {
+	navigator.camera.getPicture(onPhotoURISuccess, onFail, { 
+		quality: 20,
+		destinationType: destinationType.DATA_URL,
+		sourceType: pictureSource.PHOTOLIBRARY
+	});
+});
+var receiptPicture;
+function onPhotoURISuccess(ReceiptPhoto){
+	$('#profilepicture').attr('src', "data:image/jpeg;base64,"+ReceiptPhoto);
+	//$('#piccyoutput').val(ReceiptPhoto);
+	receiptPicture = ReceiptPhoto;
+}
+function onFail(){
+	alert("Please try selecting the file again");
+}
+var base_image = null;
 
 function refreshProfilePage(){
 	var thisapikey = localStorage.getItem("conf_"+curconference+"_apikey");
@@ -253,6 +340,92 @@ function refreshCurConference(){
 	}
 }
 
+function getAttendeeList(){
+	if(isInternet){
+		var thisapikey = localStorage.getItem("conf_"+curconference+"_apikey");
+		var data = "action=getattendees&apikey="+thisapikey+"&attendeeref="+localStorage.getItem("conf_"+curconference+"_reference");
+		$.ajax({
+			url: apiURL,
+			data: data,
+			dataType: "json",
+			type: 'post',
+		}).done(function(response){
+			if(response.success){
+				var output = "";
+				for(var i = 0;i<response.data.attendees.length;i++){
+					if(response.data.attendees[i].reference != localStorage.getItem("conf_"+curconference+"_reference")){
+						output += "<button class='personlist ui-btn ui-shadow ui-corner-all'>\n";
+						output += "<p>"+response.data.attendees[i].first_name+" "+response.data.attendees[i].last_name+"</p>";
+						output += "<div><p>"+response.data.attendees[i].organisation+"</p>";
+						output += "<h3 id='"+response.data.attendees[i].reference+"' class='clickbuttongetgetperson' >more...</h3>";
+						output += "</div></button>";
+					}
+				}
+				$('#people_list').html(output);
+			}else{
+				$('#people_list').html("Unable to get list of attendees: "+response.error);
+			}
+		});
+		
+	}else{
+		$('#people_list').html("You need to be connected to the internet to see the list of attendees.  <a href='#' onclick='javascript:getAttendeeList()'>Refresh</a>");
+	}
+}
+
+function checkUserSelected(){
+	var thisattendeeref = $('#selectedattendeeref').val();
+	if(thisattendeeref==''){
+		location.href = "#delegateFind_People";
+	}
+}
+
+function getAttendeeProfile(attendeeref){
+	$('#other_profile_error').hide();
+	if(isInternet){
+		var thisapikey = localStorage.getItem("conf_"+curconference+"_apikey");
+		var data = "action=getattendee&apikey="+thisapikey+"&attendeeref="+localStorage.getItem("conf_"+curconference+"_reference");
+		data += "&attendeeref="+attendeeref;
+		$('#selectedattendeeref').val(attendeeref);
+		$.ajax({
+			url: apiURL,
+			data: data,
+			dataType: "json",
+			type: 'post',
+		}).done(function(response){
+			if(response.success){
+				$('#other_profile_first_name').html(response.data.first_name);
+				$('#other_profile_last_name').html(response.data.last_name);
+				$('#other_profile_organisation').html(response.data.organisation);
+				$('#other_profile_job_title').html(response.data.job_title);
+				$('#other_profile_bio').html(response.data.bio);
+				if(typeof(response.data.profileimg_thumb)===null || response.data.profileimg_thumb==''|| response.data.profileimg_thumb=="null"){
+					$('#other_profile_img').attr('src', "profile_placeholder.jpeg");
+				}else{
+					$('#other_profile_img').attr('src', "https://reg.bookmein2.com/images/profile/"+response.data.conferenceid+"/"+response.data.profileimg_thumb);
+				}
+			}else{
+				$('#other_profile_error').html("Unable to get list of attendees: "+response.error);
+				$('#other_profile_first_name').html(response.data.first_name);
+				$('#other_profile_last_name').html(response.data.last_name);
+				$('#other_profile_organisation').html(response.data.organisation);
+				$('#other_profile_job_title').html(response.data.job_title);
+				$('#other_profile_bio').html(response.data.bio);
+				$('#other_profile_img').attr('src', "profile_placeholder.jpeg");
+				$('#other_profile_error').show();
+			}
+		});
+	}else{
+		$('#other_profile_error').html("You need to be connected to the internet to view user profiles");
+		$('#other_profile_first_name').html(response.data.first_name);
+		$('#other_profile_last_name').html(response.data.last_name);
+		$('#other_profile_organisation').html(response.data.organisation);
+		$('#other_profile_job_title').html(response.data.job_title);
+		$('#other_profile_bio').html(response.data.bio);
+		$('#other_profile_img').attr('src', "profile_placeholder.jpeg");
+		$('#other_profile_error').show();
+	}
+}
+
 function refreshConferenceData(conferenceid, updatecurconf){
 	//update conference details, get details of meetings
 	
@@ -300,6 +473,108 @@ function fill_conference_data(){
 	
 }
 
+function getMeetingRequests(callback){
+	$('#other_profile_error').hide();
+	if(isInternet){
+		var thisapikey = localStorage.getItem("conf_"+curconference+"_apikey");
+		var data = "action=getmeetings&apikey="+thisapikey+"&attendeeref="+localStorage.getItem("conf_"+curconference+"_reference");
+		
+		$.ajax({
+			url: apiURL,
+			data: data,
+			dataType: "json",
+			type: 'post',
+		}).done(function(response){
+			if(response.success){
+				var meetingslist = [];
+				for(var i=0;i<response.data.meetings.length;i++){
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_meeting_date", response.data.meetings[i].meeting_date);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_date_sent", response.data.meetings[i].date_sent);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_sender_message", response.data.meetings[i].sender_message);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_location", response.data.meetings[i].location);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_sender_reference", response.data.meetings[i].sender_reference);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_receiver_response", response.data.meetings[i].receiver_response);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_receiver_reference", response.data.meetings[i].receiver_reference);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_sender_first_name", response.data.meetings[i].sender_first_name);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_sender_last_name", response.data.meetings[i].sender_last_name);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_sender_organisation", response.data.meetings[i].sender_organisation);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_sender_job_title", response.data.meetings[i].sender_job_title);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_sender_bio", response.data.meetings[i].sender_bio);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_receiver_first_name", response.data.meetings[i].receiver_first_name);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_receiver_last_name", response.data.meetings[i].receiver_last_name);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_receiver_organisation", response.data.meetings[i].receiver_organisation);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_receiver_job_title", response.data.meetings[i].receiver_job_title);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_receiver_bio", response.data.meetings[i].receiver_bio);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_last_update", response.data.meetings[i].last_update);
+					localStorage.setItem("conf_"+curconference+"_meeting_"+response.data.meetings[i].id+"_status", response.data.meetings[i].status);
+					meetingslist.push(response.data.meetings[i].id);
+				}
+				localStorage.setItem("conf_"+numconferences+"_meetingslist", meetingslist.join(","));
+				if(callback!=''){
+					callback();
+				}
+			}
+		});
+	}else{
+		if(callback!=''){
+			callback();
+		}
+	}
+	
+}
+
+function updateMeetingRequestsPage(){
+	var meetinglist = localStorage.getItem("conf_"+numconferences+"_meetingslist");
+	if(meetinglist!=''){
+		var meetinglistarray = meetinglist.split(",");
+		var output = "";
+		meetinglistarray.forEach(function(item, index){
+			output += "<button id='"+item+"' class='gotomeetingbutton ui-btn ui-shadow ui-corner-all'>";
+			output += "<h3 class='left'>";
+			if(localStorage.getItem("conf_"+curconference+"_meeting_"+item+"_sender_reference") == localStorage.getItem("conf_"+curconference+"_reference")){
+				//this user is the sender
+				output += "&rArr; " + localStorage.getItem("conf_"+curconference+"_meeting_"+item+"_receiver_first_name");
+				output += " " + localStorage.getItem("conf_"+curconference+"_meeting_"+item+"_receiver_last_name");
+				output += "<br>" + localStorage.getItem("conf_"+curconference+"_meeting_"+item+"_receiver_organisation");
+			}else{
+				//this user is the receiver
+				output += "&lArr; " + localStorage.getItem("conf_"+curconference+"_meeting_"+item+"_sender_first_name");
+				output += " " + localStorage.getItem("conf_"+curconference+"_meeting_"+item+"_sender_last_name");
+				output += "<br>" + localStorage.getItem("conf_"+curconference+"_meeting_"+item+"_sender_organisation");
+			}
+			output += "</h3>";
+			output += "<h1 class='right'>"+status_symbol(localStorage.getItem("conf_"+curconference+"_meeting_"+item+"_status"))+"</h1>";
+			output += "<br style='clear:both;'>";
+			output += "<h4 class='left'>"+localStorage.getItem("conf_"+curconference+"_meeting_"+item+"_meeting_date")+"</h3>";
+			output += "<h4 class='right red'>"+localStorage.getItem("conf_"+curconference+"_meeting_"+item+"_last_update")+"</h4>";
+			output += "</button>";
+		});;
+	}else{
+		var output = "No meetings have been sent or received";
+	}
+	
+	$('#meetings_list').html(output);
+	
+}
+
+function showMeetingDetails(){
+	var selectedmeeting = $('#selectedmeeting').val();
+	var meetinglist = localStorage.getItem("conf_"+numconferences+"_meetingslist");
+	if(meetinglist!=''){
+		var meetinglistarray = meetinglist.split(",");
+		var pos = meetinglistarray.indexOf(selectedmeeting);
+		if(pos>=0){
+			
+		}else{
+			//meeting not found, redirect to meetings page
+			location.href = "#delegateManage_Meeting";
+		}
+	}else{
+		//meeting not found, redirect to meetings page
+		location.href = "#delegateManage_Meeting";
+	}
+}
+
 function clearConference(conferenceid){
 	localStorage.setItem("conf_"+conferenceid+"_active", 0);
 	
@@ -317,6 +592,33 @@ function clearConference(conferenceid){
 	localStorage.removeItem("conf_"+conferenceid+"_image");
 	localStorage.removeItem("conf_"+conferenceid+"_desc");
 	localStorage.removeItem("conf_"+conferenceid+"_name");
+	var meetinglist = localStorage.getItem("conf_"+numconferences+"_meetingslist");
+	if(meetinglist!=''){
+		var meetinglistarray = meetinglist.split(",");
+		var output = "";
+		meetinglistarray.forEach(function(item, index){
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_meeting_date");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_date_sent");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_location");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_sender_reference");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_receiver_response");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_receiver_reference");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_sender_first_name");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_sender_last_name");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_sender_organisation");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_sender_job_title");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_sender_bio");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_receiver_first_name");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_receiver_last_name");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_receiver_organisation");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_receiver_job_title");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_receiver_bio");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_last_update");
+			localStorage.removeItem("conf_"+conferenceid+"_meeting_"+item+"_status");
+		});;
+	}
+	localStorage.removeItem("conf_"+conferenceid+"_meetingslist");
+	
 }
 
 //Page change listener - calls functions to make this readable. NB due to the way the "pages" are loaded we cannot put this inside the document ready function.
@@ -335,12 +637,16 @@ $(document).on( "pagecontainerchange", function( event, ui ) {
 		case "delegateConfirmed_Meeting":
 			break;
 		case "delegateFind_People":
+			getAttendeeList();
 			break;
 		case "delegateManage_Meeting":
+			getMeetingRequests(updateMeetingRequestsPage);
 			break;
 		case "delegatePerson_Profile":
+			checkUserSelected();
 			break;
-		case "delegateReply_Meeting":
+		case "delegateMeetingDetails":
+			showMeetingDetails();
 			break;
 		case "delegateUser_Profile":
 			refreshProfilePage();
@@ -415,10 +721,35 @@ function setlastupdatedtime(){
 
 
 function hide_div(div_id) {   
-   document.getElementById(div_id).classList.toggle("hide");
+	document.getElementById(div_id).classList.toggle("hide");
 }
 
 function toggle_content(current, alternative) { 
-   hide_div(current);
-   hide_div(alternative);
+	hide_div(current);
+	hide_div(alternative);
+}
+
+//Sham -returns symbol text for a meeting status
+function status_symbol(status){
+	var statussymbol;
+			switch(status){
+			case "Accepted":
+					statussymbol = "&#10003;";
+					break;
+			case "Refused":
+			case "Declined": //To display correct text on Confirmed Meeting - The case:case: is deliberate
+					statussymbol = "&#10007;";
+					break;
+			case "Pending":
+					statussymbol = "...";
+					break;
+			case "Rearrange":
+			case "Amended"://see above comment
+					statussymbol = "?";
+					break;
+			default:
+					statussymbol = "";
+					break;
+	}
+	return statussymbol;
 }
